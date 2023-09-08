1daa329a2a94af4e8e359baf418a57d781f30d76
Emanuel Reynoso <emanuel.reynoso@swissmedical.com.ar>
2023-09-07 21:14:12 -0300

    Clase3 Faltan hacer los ejercicios

--- TestingClase2/Program.cs
+++ TestingClase2/Program.cs
@@ -8,10 +8,40 @@ namespace TestingClase2
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int primerNumero = int.Parse(Console.ReadLine());
+            //int primerNumero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa un numero"); //Aca pido 2 nums por entrada y los sumo a la fun
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Suma(num1, num2));
+            Suma(23, 56);
+            Console.WriteLine(Suma(num1, num2));
+            BuenoAdios();
+            
+
+            Console.ReadKey();
+
+
+
 
         }
+
+        public static void BuenoAdios()//Metodo de Saludar
+        {
+            Console.WriteLine("Bueno nos vemo en Disney");
+        }
+
+        public static int Suma(int num1, int num2)
+        {
+            int sumaTotal= num1 + num2;
+            return sumaTotal;
+
+        }
+
+        
+
+        
     }
 }
---
+++ TestingClase3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingClase3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Clase 3, Condiciones, if, else, switch, case, while
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Beep();
+            Console.WriteLine("\n" +
+                              "\n" +
+                              "\n" +
+                              " ---------------------------------- \n" +
+                              "| SUBE              ||             |\n" +
+                              "|                   ||             |\n" +
+                              "|                |OOOOOO|          |\n"+
+                              "|               |OO| __OO|         |\n" +
+                              "|               |OO|/  |__|        |\n" +
+                              "|                |OOOOOO|          |\n" +
+                              "|               ---    |OO|        |\n" +
+                              "|               |OO|   |OO|        |\n" +
+                              "|                |OOOOOOO|         |\n" +
+                              "|                 |_||_/           |\n" +
+                              "|                   ||             |\n" +
+                               "---------------------------------- \n"); 
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
