7f9d6e724fac8617aa099a928ac4c9e72a1209cb
Ema <reynosoemanuel04@gmail.com>
2023-09-17 21:58:50 -0300

    Falta tarea clase 3 y 4

---
+++ TareaClase1Consola/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaClase1Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tarea 1 Reynoso
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("###################################\n" +
+                              "#        TESTING AUTOMATION       #\n" +
+                              "###################################");
+            //Combine todo para que quedara mejor al final
+            Console.WriteLine("\n");
+            Console.WriteLine("¡Hola!, Ingresa tu nombre, por favor:");
+            var nombre = Console.ReadLine();
+            Console.WriteLine($"Mucho gusto, {nombre}");
+            Console.WriteLine("\n");
+            Console.WriteLine("Ingresa tu apellido, por favor:");
+            var apellido = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Necesito ahora que ingreses tu E-mail, por favor:");
+            var email = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Tambien tu fecha de nacimiento:");
+            var fecha = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Ahora que ingreses tu dirección, por favor:");
+            var direccion = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Por ultimo ingresa tu telefono, por favor:");
+            var telefono = Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Se ha registrado correctamente.");
+            Console.WriteLine("\n");
+            Console.Beep();
+            Console.WriteLine($"¡Bienvenido! Sus datos son:" +
+
+                $"\n" +
+                $"\nNombre:{nombre} " +
+                $"\nApellido: {apellido} " +
+                $"\nE-Mail: {email} " +
+                $"\nFecha de nacimiento: {fecha} " +
+                $"\nDirección:{direccion} " +
+                $"\nTelefono: {telefono} ");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TareaClase2Consola/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaClase2Consola
+{
+    class Calculadora
+    {
+        //Clase 2 Reynoso
+        //Funciones con las operaciones basicas.
+        //-------------------------------------------
+        // Método para sumar dos números
+        public static double Sumar(double a, double b)
+        {
+            return a + b;
+        }
+
+        // Método para restar dos números
+        public static double Restar(double a, double b)
+        {
+            return a - b;
+        }
+
+        // Método para multiplicar dos números
+        public static double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+
+        // Método para dividir dos números
+        public static double Dividir(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Error: No se puede dividir por cero.");
+                return double.NaN; // Dividi y .NaN para que devuelva algo
+            }
+            return a / b;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("###################################\n" +
+                              "#        TESTING AUTOMATION       #\n" +
+                              "###################################");
+            Console.WriteLine("\n");
+            // Ejemplo de uso de los métodos de la Calculadora
+            Console.WriteLine("Ingresa 2 números para realizar las 4 operaciones basicas\n" +
+                "Presiona ENTER despues de cada número que elijas.");
+            double num1 = int.Parse(Console.ReadLine());
+            double num2 = int.Parse(Console.ReadLine());
+            Console.Beep();
+            Console.WriteLine("Suma: " + Calculadora.Sumar(num1, num2));
+            Console.WriteLine("Resta: " + Calculadora.Restar(num1, num2));
+            Console.WriteLine("Multiplicación: " + Calculadora.Multiplicar(num1, num2));
+            Console.WriteLine("División: " + Calculadora.Dividir(num1, num2));
+
+
+            // Prueba de división por cero
+            double resultado = Calculadora.Dividir(num1, 0);
+
+            Console.ReadKey();
+        }
+    }
+}
