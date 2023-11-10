cc7eb6aef7a13858ccd517052002926ee4a581a3
Ema <hwa_rang04@hotmail.com>
2023-11-09 22:15:50 -0300

    Tarea de automatizacion

---
+++ TestingClase12Automation/PrimerTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingClase12Automation
+{
+    [TestFixture]
+    public class PrimerTest
+    {
+       [Test]
+       public void Test1()
+        {
+            //Primer Test
+
+
+        }
+    }
+}
